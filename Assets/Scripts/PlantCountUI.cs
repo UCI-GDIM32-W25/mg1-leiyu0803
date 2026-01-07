56869f79a2c476d31197efa26ca46338d6569371
leiyu0803 <leiyu0803@qq.com>
2026-01-06 18:49:24 -0800

    1

--- Assets/Scripts/PlantCountUI.cs
+++ Assets/Scripts/PlantCountUI.cs
@@ -6,8 +6,9 @@ public class PlantCountUI : MonoBehaviour
     [SerializeField] private TMP_Text _plantedText;
     [SerializeField] private TMP_Text _remainingText;
 
-    public void UpdateSeeds (int seedsLeft, int seedsPlanted)
+    public void UpdateSeeds(int seedsLeft, int seedsPlanted)
     {
-        
+        _plantedText.text = seedsPlanted.ToString();
+        _remainingText.text = seedsLeft.ToString();
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,29 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        _numSeedsLeft = _numSeeds;
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 
     private void Update()
     {
-        
+        _playerTransform.position += Vector3.right * Input.GetAxis("Horizontal") * _speed * Time.deltaTime;
+        _playerTransform.position += Vector3.up * Input.GetAxis("Vertical") * _speed * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if(_numSeedsLeft > 0)
+            {
+                PlantSeed();
+                _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
+            }
+        }
+
     }
 
     public void PlantSeed ()
     {
-        
+        GameObject plant = Instantiate(_plantPrefab);
+        plant.transform.position = _playerTransform.position;
+        _numSeedsLeft--;
+        _numSeedsPlanted++;
     }
 }
